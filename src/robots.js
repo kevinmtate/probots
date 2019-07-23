aa63d9017823e979eae4c7d96d4dab534485b8ab
Kevin Tate <kmtate@Kevins-MacBook-Pro.local>
2019-07-23 15:57:56 -0400

    probots unite

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Steph Curry",
+        username: "SC30",
+        email: "trueloveisgolf@dubnation.dyn"
+    },
+    {
+        id: 2,
+        name: "Klay Thompson",
+        username: "KlayBayby",
+        email: "onlychild@dubnation.dyn"
+    },
+    {
+        id: 3,
+        name: "Draymond Green",
+        username: "DrayBayby",
+        email: "dpoy@dubnation.dyn"
+    },
+    {
+        id: 4,
+        name: "LeBron James",
+        username: "KingJames",
+        email: "kingjames@kingjames.kingjames"
+    },
+    {
+        id: 5,
+        name: "Kevin Durant",
+        username: "donthurtme",
+        email: "notasnake@snakes.gov"
+    },
+    {
+        id: 6,
+        name: "Kawhi Leonard",
+        username: "ThaClaw",
+        email: "boardman000@nba.com"
+    },
+    {
+        id: 7,
+        name: "James Harden",
+        username: "lefty",
+        email: "thatwasafoul@rockets.net"
+    },
+    {
+        id: 8,
+        name: "Anthony Davis",
+        username: "brow",
+        email: "thebrow@lakers.net"
+    },
+    {
+        id: 9,
+        name: "Giannis Antetokounmpo",
+        username: "GreekFreak",
+        email: "iammyfatherslegacy@greece.gov"
+    },
+    {
+        id: 10,
+        name: "Russell Westbrook",
+        username: "tripledouble",
+        email: "gimmethaball@222.com"
+    },
+    {
+        id: 11,
+        name: "Paul George",
+        username: "PG13",
+        email: "pg13@nba.com"
+    }
+]
